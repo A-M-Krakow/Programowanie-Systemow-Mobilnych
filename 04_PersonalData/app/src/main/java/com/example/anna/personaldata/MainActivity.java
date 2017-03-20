33f63083055177aa518a8eb7eed395cb43e85c97
Anna Madej <aniamadej123@gmail.com>
2017-03-20 13:42:34 +0100

    PersonalData z dialogBoxem = wszystkie dane

--- 04_PersonalData/app/src/main/java/com/example/anna/personaldata/MainActivity.java
+++ 04_PersonalData/app/src/main/java/com/example/anna/personaldata/MainActivity.java
@@ -17,13 +17,27 @@ public class MainActivity extends AppCompatActivity {
     public void showDialog(View view) {
 
         EditText imieTextView = (EditText) findViewById(R.id.imieTextView);
+        EditText nazwiskoTextView = (EditText) findViewById(R.id.nazwiskoTextView);
+        Spinner plecSpinner = (Spinner) findViewById(R.id.plecSpinner);
+        EditText adresTextView = (EditText) findViewById(R.id.adresTextView);
+        EditText kodTextView = (EditText) findViewById(R.id.kodTextView);
+        EditText miastoTextView = (EditText) findViewById(R.id.miastoTextView);
+
+
         String imie = imieTextView.getText().toString();
+        String nazwisko = nazwiskoTextView.getText().toString();
+        String plec = plecSpinner.getSelectedItem().toString();
+        String adres = adresTextView.getText().toString();
+        String kod = kodTextView.getText().toString();
+        String miasto = miastoTextView.getText().toString();
+
 
         AlertDialog alertDialog =   new AlertDialog.Builder(MainActivity.this).create();
         alertDialog.setTitle(getResources().getString(R.string.dialogTitle));
-        String message = imie + ",  " + getResources().getString(R.string.dialogText);
+        String message = imie + "\n" + nazwisko + "\n" + plec + "\n" + adres + "\n" + kod + " " + miasto + "\n" + getResources().getString(R.string.dialogText);
         alertDialog.setMessage(message);
         alertDialog.show();
+
 
 
 
@@ -35,11 +49,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Spinner spinner = (Spinner) findViewById(R.id.plec);
+        Spinner plecSpinner = (Spinner) findViewById(R.id.plecSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.plec, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        plecSpinner.setAdapter(adapter);
 
 
 
