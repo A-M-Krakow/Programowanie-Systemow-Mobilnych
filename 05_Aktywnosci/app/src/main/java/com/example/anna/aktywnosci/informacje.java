c043114b0c3fd9023a62125158c5621cdd741d22
Anna Madej <aniamadej123@gmail.com>
2017-03-21 20:43:20 +0100

    Aktywności - aktywacja przycisku pokazInformacje

---
+++ 05_Aktywnosci/app/src/main/java/com/example/anna/aktywnosci/informacje.java
@@ -0,0 +1,13 @@
+package com.example.anna.aktywnosci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class informacje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacje);
+    }
+}
