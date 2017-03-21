409e5cbc1e56ee94bd33e8f3ba70bf4fda94f006
Anna Madej <aniamadej123@gmail.com>
2017-03-21 23:27:07 +0100

    Konwerter

---
+++ 06_Konwerter/app/src/main/java/com/example/anna/konwerter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.anna.konwerter;
+
+import android.icu.math.BigDecimal;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    final static Double KURS = 4.27;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void przelicz (View view)
+    {
+        TextView EURTextView = (TextView) findViewById(R.id.EURTextView);
+        EditText PLNEditText = (EditText) findViewById(R.id.PLNEditText);
+        String PLN = PLNEditText.getText().toString();
+
+        Double EURDOuble = KURS * (new Double (PLN));
+
+        String EUR = EURDOuble.toString();
+
+
+        EURTextView.setText(PLN + " PLN = " + EUR + " EUR");
+    }
+}
